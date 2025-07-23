c218ca25525b096da50018399fb282400980b235
SysFAB <sysfab64@gmail.com>
2025-07-23 19:16:42 +0300

    Update

--- install.sh
+++ install.sh
@@ -20,26 +20,12 @@ echo "Applying config..."
 mkdir -p ~/.config/hypr
 mv $HOME/dotfiles/install/hyprland-install.conf $HOME/.config/hypr/hyprland.conf
 
-echo "Installing hyprpaper, waybar, wofi, hyprshot, wlogout..."
-sudo pacman -S hyprpaper waybar wofi hyprshot --noconfirm
-yay -S wlogout --noconfirm
-
 echo "Installing fonts..."
 sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra --noconfirm
 sudo pacman -S otf-font-awesome --noconfirm
 
-echo "Installing pulseaudio..."
-sudo pacman -S pulseaudio --noconfirm
-
-echo "Installing apps..."
-sudo pacman -S kitty nautilus --noconfirm
-#yay -S google-chrome --noconfirm
-
-echo "Installing scrcpy..."
-#sudo pacman -S scrcpy --noconfirm
-
-echo "Installing coppwr..."
-# yay -S coppwr --noconfirm
+echo "Installing kitty..."
+sudo pacman -S kitty --noconfirm
 
 echo "Launching visual install..."
 Hyprland
--- install/install-visual.sh
+++ install/install-visual.sh
@@ -1,6 +1,29 @@
 echo "Please wait..."
 sleep 1
 
+MONITOR_NAMES=$(echo "$MONITORS_JSON" | jq -r '.[].name')
+
+for MONITOR_NAME in $MONITOR_NAMES; do
+    hyprctl hyprpaper wallpaper "$MONITOR_NAME,$HOME/dotfiles/hyprpaper/wallpapers/wallpaper1.jpg"
+done
+
+echo "Installing pulseaudio..."
+#sudo pacman -S pulseaudio --noconfirm
+
+echo "Installing waybar, wofi, hyprshot, wlogout..."
+sudo pacman -S waybar wofi hyprshot --noconfirm
+yay -S wlogout --noconfirm
+
+echo "Installing apps..."
+sudo pacman -S nautilus --noconfirm
+#yay -S google-chrome --noconfirm
+
+echo "Installing scrcpy..."
+#sudo pacman -S scrcpy --noconfirm
+
+echo "Installing coppwr..."
+# yay -S coppwr --noconfirm
+
 echo "Configuring hyprland monitors..."
 bash $HOME/dotfiles/install/create_monitors_config.sh
 
